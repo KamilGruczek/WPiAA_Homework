f07fb9a5493449382cbc53634b351262a641aa73
Kamil Gruczek <kamil.gruczek@outlook.com>
2025-11-12 23:23:53 +0100

    Singleton

---
+++ SingletonVault/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingletonVault;
+
+var _value1 = Vault.Instance.GetSecretKey();
+var _value2 = Vault.Instance.GetSecretKey();
+
+Console.WriteLine($"Secret Key 1: {_value1}");
+Console.WriteLine($"Secret Key 2: {_value2}");
+Console.WriteLine($"Are both keys equal? {_value1 == _value2}");
---
+++ SingletonVault/Vault.cs
@@ -0,0 +1,43 @@
+﻿namespace SingletonVault
+{
+    internal class Vault
+    {
+        private readonly string _secretKey;
+        private static Vault? _vault;
+        private static object _lock = new object();
+
+        private Vault()
+        {
+            _secretKey = GenerateSecretKey();
+        }
+
+        public static Vault Instance
+        {
+            get
+            {
+                if (_vault == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_vault == null)
+                        {
+                            _vault = new Vault();
+                        }
+                    }
+                }
+
+                return _vault;
+            }
+        }
+
+        public string GetSecretKey()
+        {
+            return _secretKey;
+        }
+
+        private static string GenerateSecretKey()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
